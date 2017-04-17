2ff9fc0f252e7017d09e4b0c014c6fad7ef70504
Wu <cool425589@gamil.com>
2017-04-18 00:11:43 +0800

    sessionn 修改中

--- logout.php
+++ logout.php
@@ -2,7 +2,15 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
 //將session清空
-unset($_SESSION['username']);
+$msg="" ;
+$file = "message2.html" ;
+$username = $_SESSION["username"];
+$msg .="<p>使用者:". $username ."登出</p>" ;
+$fp = fopen($file, "a+");
+fwrite($fp, $msg);
+fclose($fp);
+session_destroy();
 echo '登出中......';
+
 echo '<meta http-equiv=REFRESH CONTENT=1;url=login.php>';
 ?>
--- success.php
+++ success.php
@@ -13,6 +13,7 @@
 	}
 	else
 	{
+		
 		header("location:login.php") ;
 	}
 
